7e484d3d8f8506e163bcc8aa95508f2120a69a3e
SAM <8dmasters@gmail.com>
2024-12-14 00:31:44 +0530

    Add login page

---
+++ src/main/java/com/example/campuscheck/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.example.campuscheck;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+
+        if (username.equals("username") && password.equals("password")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            session.setAttribute("role", role);
+            response.sendRedirect("index.jsp");
+        } else {
+            response.sendRedirect("login.html");
+        }
+    }
+}
