f6cab71090da5065402a214bd2b6a975c469f8ab
SAM <8dmasters@gmail.com>
2024-12-14 19:35:17 +0530

    Removed unused variable username

--- src/main/java/com/example/campuscheck/HomeServlet.java
+++ src/main/java/com/example/campuscheck/HomeServlet.java
@@ -14,7 +14,6 @@ public class HomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
         if (session != null && session.getAttribute("role") != null) {
-            String username = (String) session.getAttribute("username");
             String role = (String) session.getAttribute("role");
 
             switch (role) {
